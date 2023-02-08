3e43a53b125e8f851df632e33a256f2aa47a7827
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-02-08 09:31:58 -0600

    ESLint fix

--- pages/scripts/artists.js
+++ pages/scripts/artists.js
@@ -4,7 +4,7 @@ const artists = db;
 
 document.getElementById('artists').innerHTML = `
 <h1 class="sectionTitle">Performers</h1>
-${artists.map((artist, i) => `<article class="artist-card">
+${artists.map((artist) => `<article class="artist-card">
 <img src="${artist.image}" alt="${artist.alt}" class="artist-img">
 <dl class="artist-data">
   <h1><dt>${artist.name}</dt></h1>
