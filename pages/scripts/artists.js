8d541a4864f6ee84dd7f49c2b6c58577d3a34d04
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-02-08 09:44:21 -0600

    stylelint fix

--- pages/scripts/artists.js
+++ pages/scripts/artists.js
@@ -7,9 +7,9 @@ document.getElementById('artists').innerHTML = `
 ${artists.map((artist) => `<article class="artist-card">
 <img src="${artist.image}" alt="${artist.alt}" class="artist-img">
 <dl class="artist-data">
-  <h1><dt>${artist.name}</dt></h1>
+  <h1 class="artist-name"><dt>${artist.name}</dt></h1>
   <dd>${artist.description}</dd>
-  <p>${artist.intro}</p>
+  <p class="artist-intro">${artist.intro}</p>
 </dl>
 </article>`)
 }`;
