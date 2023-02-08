9342ab290d7be0fb83c806cf8444dfec4bf2d9d7
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-02-07 18:26:55 -0600

    Add #artists section render with javascript

---
+++ db.js
@@ -0,0 +1,11 @@
+const artists = [
+  {
+    name: 'Interpol',
+    image: '/images/content/interpol.jpg',
+    alt: 'Paul Banks, Daniel Kessler and Sam Fogarino dress up in suits, full body shot.',
+    description: 'Interpol is an American rock band from Manhattan, New York. Formed in 1997.',
+    intro: 'Interpol is one of the bands associated with the New York indie music scene and one of several groups that emerged from the post-punk revival of the 2000s.',
+  },
+];
+
+export default artists;
