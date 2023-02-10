b47525262108ec301c7fbe2c7d62f7958b280965
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-02-10 13:33:07 -0600

    menu fix

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,5 @@
 const checkbox = document.getElementById('checkbox');
 
-if (window.screen.width > 767) {
+if (window.screen.width > 768) {
   checkbox.disabled = true;
 }
-
-Array.from(document.getElementsByClassName('nav-link'))
-  .forEach((item) => item.addEventListener('click', () => { checkbox.checked = false; }));
